d3d32bd64774e0c08077e8eb7979c668ad31dc5a
Evgenii Morozov <evgeny.a.morozov@gmail.com>
2026-04-01 23:45:19 +0300

    Поправил зум и перемещение стола мышью. Теперь зум происходит в курсор. Перемещение имеют постоянную скорость.

--- scripts/other/camera_2d.gd
+++ scripts/other/camera_2d.gd
@@ -1,24 +1,16 @@
 extends Camera2D
 
-# Настройки зума
-var zoom_speed = 0.1
-var min_zoom = 0.8
-var max_zoom = 10
-var zoom_duration = 0.2
-@onready var target_zoom = zoom
+var zoom_speed = 0.2
 
 # Состояние перетаскивания
 var is_dragging = false
 
 func _unhandled_input(event):
-	# пока зум отключен
-	#return
-	# Логика ЗУМА
 	if event.is_action_pressed("zoom_out"):
-		zoom_to(target_zoom - Vector2(zoom_speed, zoom_speed))
+		modify_zoom(-zoom_speed)
 	elif event.is_action_pressed("zoom_in"):
-		zoom_to(target_zoom + Vector2(zoom_speed, zoom_speed))
-
+		modify_zoom(zoom_speed)
+	
 	# Логика НАЖАТИЯ колесика (MMB)
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MIDDLE:
 		is_dragging = event.pressed
@@ -27,15 +19,13 @@ func _unhandled_input(event):
 
 	# Логика ДВИЖЕНИЯ мыши
 	if event is InputEventMouseMotion and is_dragging:
-		# Сдвигаем камеру против движения мыши, учитывая текущий зум
-		# Умножение на zoom позволяет камере двигаться адекватно при любом масштабе
-		position -= event.relative * zoom
+		# С коэффициентом 1.1 оно работает лучше
+		position -= event.screen_relative / zoom * 1.1
 
-func zoom_to(new_zoom: Vector2):
-	target_zoom.x = clamp(new_zoom.x, min_zoom, max_zoom)
-	target_zoom.y = clamp(new_zoom.y, min_zoom, max_zoom)
-	
-	%player_locations.global_position = Vector2(randf_range(0,100), randf_range(0, 100))
-	
-	var tween = create_tween()
-	tween.tween_property(self, "zoom", target_zoom, zoom_duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+## Zoom'ится в курсор мыши
+func modify_zoom(delta: float) -> void:
+	var mouse_pos := get_global_mouse_position()
+	zoom += Vector2(delta, delta)
+
+	var new_mouse_pos := get_global_mouse_position()
+	position += mouse_pos - new_mouse_pos
