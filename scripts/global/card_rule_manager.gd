65f3d095b18ea522661d6fa60a15845a5dd7023b
Evgenii Morozov <evgeny.a.morozov@gmail.com>
2026-04-04 18:04:48 +0300

    Добавил правил, запрещающих поведение вокруг карты Кладбища (LOCATION)

--- scripts/global/card_rule_manager.gd
+++ scripts/global/card_rule_manager.gd
@@ -1,27 +1,38 @@
 class_name CardRuleManager extends Node
 
 
-const CardType = DataManager.CardType
+const CT = DataManager.CardType
 
-## Для ключа запрещено всё, что в его списке, т.е. в 
-## - в PRODUCTION нельзя положить PRODUCTION
+## В ключ запрещено класть всё, что в его списке, т.е. в 
+## - в PRODUCTION нельзя положить PRODUCTION и LOCATION
 ## - в MONSTER нельзя положить LOCATION и PRODUCTION
 const FORBIDDEN_FOR_STACK = {
-	CardType.PRODUCTION: [CardType.PRODUCTION],
-	CardType.LOCATION: [CardType.PRODUCTION],
-	CardType.MONSTER: [CardType.LOCATION, CardType.PRODUCTION]
+	CT.PRODUCTION: [CT.PRODUCTION, CT.LOCATION],
+	CT.MONSTER: [CT.LOCATION, CT.PRODUCTION],
+	CT.LOCATION: [CT.PRODUCTION, CT.LOCATION], 
+	CT.MONSTER_PART: [CT.LOCATION], 
+	CT.ENVIRONMENT: [CT.LOCATION], 
+	CT.RES: [CT.LOCATION], 
+	CT.ITEM: [CT.LOCATION], 
+	CT.RECEPT: [CT.LOCATION], 
+	CT.NPC: [CT.LOCATION], 
+	CT.ORDER: [CT.PRODUCTION, CT.LOCATION, CT.UPGRADE], 
+	CT.UPGRADE: [CT.LOCATION]
 }
 
 const FORBIDDEN_TO_BE_MOVED = [
-	CardType.ORDER
+	CT.ORDER
 ]
 
 # Карта b помещается на карту a
 static func can_stack(card_a: Card, card_b: Card) -> bool:
 	var forbidden_types = FORBIDDEN_FOR_STACK.get(card_a.card_type, [])
-	if card_b.card_type in forbidden_types: return false
+	if card_b.card_type in forbidden_types: 
+		print_rich("[color=red]DEBUG: FORBIDDEN[/color] ", card_b, ' into ', card_a)
+		return false
 	
 	# Пока что всё остальное можно
+	print_rich("[color=green]DEBUG: ALLOWED[/color] ", card_b, ' into ', card_a)
 	return true
 
 static func forbidden_to_be_moved(card: Card) -> bool:
